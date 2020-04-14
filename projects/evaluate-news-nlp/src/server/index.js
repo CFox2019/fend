2fb43e60279c00954c7ed4dbd9b6e226d359859d
CFox2019 <57727915+CFox2019@users.noreply.github.com>
2020-04-13 21:06:14 -0400

    added aylien sdk

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,27 +1,30 @@
-var path = require('path')
 const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+const mockAPIResponse = require('./mockAPI')
+const aylien = require('aylien_textapi')
+const dotenv = require('dotenv');
+dotenv.config();
+
+const textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+})
 
 const app = express()
 
-// Cors for cross origin allowance
-const cors = require('cors');
-app.use(cors());
+const cors = require('cors')
+app.use(cors())
 
 app.use(express.static('dist'))
 
-console.log(__dirname)
-
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
-    // res.sendFile(path.resolve('src/client/views/index.html'))
-})
-
-// designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
 })
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
+})
+
+const port = 8080
+app.listen(port, function () {
+    console.log(`News Article NLP app listening on port ${port}!`)
 })
